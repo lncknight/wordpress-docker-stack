58aca5f3db1704a2e7e9ac2e323b66139cbefac7
Your Name <you@example.com>
2021-01-26 00:50:02 +0000

    bacup uploads

--- docker_wp/scripts/backup.js
+++ docker_wp/scripts/backup.js
@@ -6,10 +6,13 @@ let { chain, trim } = require('lodash')
 let moment = require('moment')
 let bb = require('bluebird')
 let {
+  BACKUP_S3_WP_UPLOADS_PREFIX,
+
   BACKUP_S3_WP_DATA_PREFIX,
   BACKUP_S3_BUCKET,
   BACKUP_DISABLE,
   BACKUP_WP_EXCLUDE_FOLDERS,
+
   TEST_RUN_CRON,
 } = process.env
 
@@ -62,8 +65,56 @@ let backup = async () => {
   })
 }
 
+let backupWpUploads = async () => {
+
+  if (BACKUP_DISABLE === "1"){
+    return 
+  }
+
+  if (!BACKUP_S3_WP_UPLOADS_PREFIX){
+    return
+  }
+
+  let folder = moment().format('YMMDD_HHmmss')
+  let backupDir = `/tmp/${folder}`
+  let zipPath = `/tmp/${folder}.tar.gz`
+  let s3Url = `s3://${BACKUP_S3_BUCKET}/${BACKUP_S3_WP_UPLOADS_PREFIX}/${folder}.tar.gz`
+
+
+  let commandParts = []
+  commandParts.push(`rsync -rhp`)
+  // --exclude="wp-content/updraft"  
+  commandParts.push(`/var/www/html/wp-content/uploads/ ${backupDir}`)
+  let command = commandParts.join(' ')
+  console.log('copying..', { command })
+  await execSync(command)
+  // await execSync(`rsync -rhp --exclude="wp-content" /var/www/html/ ${backupDir}`)
+
+  console.log('zipping..')
+  await execSync(`tar -zcf ${zipPath} ${backupDir}`)
+
+  console.log('uploading..')
+  await execSync(`/usr/local/bin/aws s3 cp ${zipPath} ${s3Url}`)
+
+  console.log('cleaning up..')
+  await bb.all([
+    execSync(`rm -rf ${backupDir}`),
+    execSync(`rm -rf ${zipPath}`),
+  ])
+
+  console.log('DONE')
+
+  console.log({
+    folder,
+    backupDir,
+    zipPath,
+    s3Url
+  })
+}
+
 // backup() // test now
 TEST_RUN_CRON === "1" && new CronJob('*/45 * * * * *', backup, null, true, 'Asia/Hong_Kong'); // test run
+TEST_RUN_CRON === "1" && new CronJob('*/45 * * * * *', backupWpUploads, null, true, 'Asia/Hong_Kong'); // test run
 
 // live
 new CronJob('0 13 * * *', backup, null, true, 'Asia/Hong_Kong');
